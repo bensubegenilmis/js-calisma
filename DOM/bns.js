730d17affe9940fc346c0692512e0ff68ee47495
Bensu Beğenilmiş <bensubegenilmiss@gmail.com>
2024-04-01 16:35:24 +0300

    DOM

---
+++ DOM/bns.js
@@ -0,0 +1,22 @@
+// const hata= document.querySelector("p");
+
+// const hata= document.querySelector(".error");
+
+// const hata= document.querySelector("div.error");
+
+// console.log(hata);
+
+const hatalar = document.querySelectorAll("p");
+console.log(hatalar);
+console.log(hatalar[1]);
+
+hatalar.forEach(hata =>
+    {
+        console.log(hata);
+    })
+
+const baslik = document.getElementById("can");
+console.log(baslik);
+
+const hatalarr=document.getElementsByClassName("error");
+console.log(hatalarr);
