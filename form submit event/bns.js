85ea1919c4f9f6285b86540027390397cbecccf4
Bensu Beğenilmiş <bensubegenilmiss@gmail.com>
2024-04-03 12:03:14 +0300

    3.04

--- form submit event/bns.js
+++ form submit event/bns.js
@@ -8,3 +8,23 @@ form.addEventListener("submit", e=>{
     console.log(form.username.value);
 
 })
+
+
+const username1= "bnsbns"; 
+const pattern=/^[a-z]{6,10}$/;
+
+// let sonuc = pattern.test(username1);
+// //console.log(sonuc);
+
+// if(sonuc)
+// {
+//     console.log("Başarılı");
+// }
+// else
+// {
+//     console.log("Başarısız");
+// }
+
+let sonuc= username1.search(pattern);
+console.log(sonuc);
+
