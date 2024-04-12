21455849a369f8ee2c9ca3fd2b869f593c978580
Bensu Beğenilmiş <bensubegenilmiss@gmail.com>
2024-04-12 14:10:51 +0300

    pushandpop

---
+++ Methods/PushandPopMethod/bns.js
@@ -0,0 +1,13 @@
+const points = [70, 75, 25, 35, 10, 80, 27];
+
+points.push(90);  // sona eleman ekler
+
+console.log(points);
+
+points.pop();  //sondaki elemanı çıkartır
+
+console.log(points);
+
+points.pop();
+
+console.log(points);
