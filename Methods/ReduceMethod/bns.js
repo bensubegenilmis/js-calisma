f2df161dd13856b42fe816c1fa3f43f9569a1a97
Bensu Beğenilmiş <bensubegenilmiss@gmail.com>
2024-04-12 14:01:05 +0300

    reduce

--- Methods/ReduceMethod/bns.js
+++ Methods/ReduceMethod/bns.js
@@ -1,7 +1,33 @@
-const points = [ 70, 75, 25, 35, 10, 80, 27];
+const points = [70, 75, 25, 35, 10, 80, 27];
 
 
-const result = points.reduce((value,point) => {
+const result = points.reduce((value, point) => {
+    if (point > 50) {
+        value++;
+    }
+    return value;
 
-},0)
+}, 0);
 
+console.log(result);
+
+const students = [
+    {name: "can", point: 40},
+    {name: "bns", point: 60},
+    {name: "volkan", point: 30},
+    {name: "sevinc", point: 100},
+    {name: "ali", point: 40},
+    {name: "can", point: 70},
+    {name: "ayşe", point: 20},
+    {name: "furkan", point: 100},
+];
+
+const canTotal= students.reduce((value,student)=>{
+    if(student.name == "can")
+    {
+        value+=student.point;
+    }
+    return value;
+},0);
+
+console.log(canTotal);
